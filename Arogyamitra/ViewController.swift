fd56deff425adf403f9037bb86c869441e49368f
Sangram Powar <sangram.powar@macbook-pro-3.local>
2018-12-07 19:55:07 +0530

    Initial Commit

---
+++ Arogyamitra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arogyamitra
+//
+//  Created by Sangram Powar on 07/12/18.
+//  Copyright © 2018 Nitin Landge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
