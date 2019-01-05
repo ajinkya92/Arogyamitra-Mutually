b2fba381fb52c8fadfb91d89762c385b3d097d50
Ajinkya Sonar <ajinkya_92@yahoo.com>
2019-01-06 00:31:34 +0530

    Map Implementation Complete on Gymnasium Details View Controller.

--- Arogyamitra/Controllers/GymnasiumDetailsVC.swift
+++ Arogyamitra/Controllers/GymnasiumDetailsVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Kingfisher
+import MapKit
 
 class GymnasiumDetailsVC: UIViewController {
     
@@ -27,6 +28,7 @@ class GymnasiumDetailsVC: UIViewController {
     @IBOutlet weak var selectPlanPickerView: UIPickerView!
     @IBOutlet weak var seeServicesBtn: UIButton!
     @IBOutlet weak var seeReviewsBtn: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
     
     //Outlets That Hides
     @IBOutlet weak var imageCollectionStackView: UIStackView!
@@ -49,6 +51,9 @@ class GymnasiumDetailsVC: UIViewController {
     
     //Variables
     var selectedPlanValue = String()
+    let annotation = MKPointAnnotation()
+    var latitudeString = String()
+    var longitudeString = String()
     
     //Strorage Variables
     var gymnasiumDetailsArray = [GymnasiumDetailsServiceResult]()
@@ -157,12 +162,68 @@ extension GymnasiumDetailsVC: UIPickerViewDelegate, UIPickerViewDataSource {
     
 }
 
+//MARK: MAP METHODS IMPLEMENTATION
+extension GymnasiumDetailsVC {
+    
+    func setMapAnnotationDetails()  {
+        
+        let latitude = (latitudeString as NSString).doubleValue
+        let longitude = (longitudeString as NSString).doubleValue
+        
+        annotation.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        mapView.addAnnotation(annotation)
+        
+        let latDelta:CLLocationDegrees = 0.05
+        
+        let lonDelta:CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: false)
+        
+    }
+
+    
+    @IBAction func btn_plusPressed(_ sender: Any) {
+        let region = MKCoordinateRegion(center: self.mapView.region.center, span: MKCoordinateSpan(latitudeDelta: mapView.region.span.latitudeDelta*0.7, longitudeDelta: mapView.region.span.longitudeDelta*0.7))
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func btn_minusPressed(_ sender: Any) {
+        let zoom = getZoom()
+        if zoom > 3.5{
+            let region = MKCoordinateRegion(center: self.mapView.region.center, span: MKCoordinateSpan(latitudeDelta: mapView.region.span.latitudeDelta/0.7, longitudeDelta: mapView.region.span.longitudeDelta/0.7))
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    func getZoom() -> Double {
+        var angleCamera = self.mapView.camera.heading
+        if angleCamera > 270 {
+            angleCamera = 360 - angleCamera
+        } else if angleCamera > 90 {
+            angleCamera = fabs(angleCamera - 180)
+        }
+        let angleRad = Double.pi * angleCamera / 180
+        let width = Double(self.view.frame.size.width)
+        let height = Double(self.view.frame.size.height)
+        let heightOffset : Double = 20
+        let spanStraight = width * self.mapView.region.span.longitudeDelta / (width * cos(angleRad) + (height - heightOffset) * sin(angleRad))
+        return log2(360 * ((width / 256) / spanStraight)) + 1
+    }
+    
+}
+
 
 // MARK: API CALL TO GET GYM DETAILS
 extension GymnasiumDetailsVC {
     
     func getGymnasiumDetails() {
         
+        self.view.makeToastActivity(.center)
         guard let gymnasiumId = gymnasiumId else {return}
         
         GymnasiumServices.instance.getGymnasiumDetails(withGymnasiumId: gymnasiumId, andPatientId: patientId) { (success, returnedGymnasiumDetails) in
@@ -181,6 +242,8 @@ extension GymnasiumDetailsVC {
                 self.selectPlanPickerView.reloadAllComponents()
                 self.seeServicesTblView.reloadData()
                 self.seeReviewsTblView.reloadData()
+                self.setMapAnnotationDetails()
+                self.view.hideToastActivity()
             }
             
         }
@@ -211,6 +274,8 @@ extension GymnasiumDetailsVC {
             self.seeServicesBtn.setTitle("See Services: (\((self.gymnasiumServicesArray.count)))", for: .normal)
             self.gymnasiumReviewListArray = allValues.reviewsList
             self.seeReviewsBtn.setTitle("See Reviews: \(allValues.totalReviews)", for: .normal)
+            self.latitudeString = allValues.latitude
+            self.longitudeString = allValues.longitude
             
             //Condition to hide reviews table in no reviews
             if allValues.totalReviews == 0 {
